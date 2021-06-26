6c03e2ecfdfa200ab69e32cc7e4ca13e819d450a
Artur Radziukhin <sftawitkakpro@gmail.com>
2021-06-26 16:14:06 +0300

    init: create project, init vkSDK

---
+++ vkTest/vkTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vkTest
+//
+//  Created by Artur Radziukhin on 26.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
